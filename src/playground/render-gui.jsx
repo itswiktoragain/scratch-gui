520a3aeefc943908c33ceb9df82f265558f623c1
itswiktoragain <propalnie15@gmail.com>
2026-08-21 04:09:10 +0200

    Remove upstream cloud default

--- src/playground/render-gui.jsx
+++ src/playground/render-gui.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import GUI from '../containers/gui.jsx';
 
 const searchParams = new URLSearchParams(location.search);
-const cloudHost = searchParams.get('cloud_host') || 'wss://clouddata.turbowarp.org';
+const cloudHost = searchParams.get('cloud_host');
 
 const RenderGUI = props => (
     <GUI
-        cloudHost={cloudHost}
-        canUseCloud
-        hasCloudPermission
+        cloudHost={cloudHost || undefined}
+        canUseCloud={Boolean(cloudHost)}
+        hasCloudPermission={Boolean(cloudHost)}
         canSave={false}
         basePath={process.env.ROOT}
         canEditTitle
